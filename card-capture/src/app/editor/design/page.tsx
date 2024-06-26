0af789a514304d7d0f6a6398483e8470aa01c580
yui880 <kimyuna521@gmail.com>
2024-06-26 15:54:21 +0900

    feat(Button): 버튼 공통 컴포넌트 구현
    패딩과 너비만 주어지면 버튼 제작 가능

---
+++ card-capture/src/app/editor/design/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Button from '@/components/common/Button';
+
+const EditorPage = () => {
+  return (
+    <>
+      <Button type="full" className="py-2.5 w-[80px] rounded-[10px]">
+        안녕
+      </Button>
+    </>
+  );
+};
+
+export default EditorPage;
---
+++ card-capture/src/components/common/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type ButtonProps = {
+  type: 'default' | 'full';
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  children: React.ReactNode;
+};
+
+const Button = ({ type, className, onClick, children }: ButtonProps) => {
+  return (
+    <button
+      className={`${type === 'default' ? 'border-[1.5px] border-main' : 'bg-main'} flex flex-row justify-center items-center rounded-[8px] ${className}`}
+      onClick={onClick}
+    >
+      <p
+        className={`${type === 'default' ? 'text-main' : 'text-white'} font-bold`}
+      >
+        {children}
+      </p>
+    </button>
+  );
+};
+
+export default Button;
