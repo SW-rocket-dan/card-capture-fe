17f50375cddbd826323247b6b379b8703457f23a
yui880 <kimyuna521@gmail.com>
2024-07-02 12:13:44 +0900

    feat(ColorPicker): 색상코드 입력해서 색상 변경하는 기능 추가
    ColorPicker와 연결해서 구현

--- card-capture/src/components/editor/Tab/TextEditBox/components/StyleBox/components/ColorButton.tsx
+++ card-capture/src/components/editor/Tab/TextEditBox/components/StyleBox/components/ColorButton.tsx
@@ -1,6 +1,7 @@
 import ColorPicker from '@/components/common/ColorPicker/ColorPicker';
 import { useState } from 'react';
 import { useColor } from 'react-color-palette';
+import CloseIcon from '@/components/common/Icon/CloseIcon';
 
 const ColorButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -21,8 +22,17 @@ const ColorButton = () => {
       </div>
 
       {isOpen && (
-        <div className="absolute mt-[10px]">
-          <ColorPicker color={color} setColor={setColor} onClick={openHandler} />
+        <div
+          className="absolute mt-[10px] rounded-lg bg-white"
+          style={{ boxShadow: '0px 2px 10px 0px rgba(0, 0, 0, 0.08' }}
+        >
+          <div className="flex flex-row justify-between px-[15px] py-[10px] font-extrabold">
+            <p className="text-md">색상</p>
+            <button onClick={openHandler}>
+              <CloseIcon width={10} className="text-gray2" />
+            </button>
+          </div>
+          <ColorPicker color={color} setColor={setColor} />
         </div>
       )}
     </div>
