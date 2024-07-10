ef388dc03c0ded9b3ffc9c8a77a1c10a7c56055a
yui880 <kimyuna521@gmail.com>
2024-07-10 17:52:30 +0900

    refactor(NavigationBar): 네비게이션바 수정

--- card-capture/src/components/common/NavigationBar/BeforeLoginNav.tsx
+++ card-capture/src/components/common/NavigationBar/BeforeLoginNav.tsx
@@ -3,17 +3,17 @@ import Button from '@/components/common/Button/Button';
 const BeforeLoginNav = () => {
   return (
     <div className="flex flex-row items-center gap-[60px]">
-      <ul className="flex flex-row gap-[50px] font-medium">
+      <ul className="text-md flex flex-row gap-[50px] text-sm">
         <li>Home</li>
         <li>다른 템플릿 보기</li>
         <li>요금제</li>
       </ul>
       <div className="flex flex-row gap-2.5">
-        <Button type="default" className="h-[44px] w-[120px] rounded-[10px]">
-          <p className="text-[16px]">Login</p>
+        <Button type="default" className="h-[40px] w-[110px] rounded-[10px]">
+          <p className="text-[14px]">Login</p>
         </Button>
-        <Button type="full" className="h-[44px] w-[120px] rounded-[10px]">
-          <p className="text-[16px]">제작하기</p>
+        <Button type="full" className="h-[40px] w-[110px] rounded-[10px]">
+          <p className="text-[14px]">제작하기</p>
         </Button>
       </div>
     </div>
--- card-capture/src/components/common/NavigationBar/NavigationBar.tsx
+++ card-capture/src/components/common/NavigationBar/NavigationBar.tsx
@@ -2,8 +2,8 @@ import BeforeLoginNav from '@/components/common/NavigationBar/BeforeLoginNav';
 
 const NavigationBar = () => {
   return (
-    <div className="fixed left-0 top-0 flex min-h-[70px] w-full items-center justify-between border-b-[1px] border-border bg-white px-[30px]">
-      <div className="text-lg">Card Capture</div>
+    <div className="fixed left-0 top-0 flex min-h-[65px] w-full items-center justify-between border-b-[1px] border-border bg-white px-[30px]">
+      <div className="text-md">Card Capture</div>
       <BeforeLoginNav />
     </div>
   );
