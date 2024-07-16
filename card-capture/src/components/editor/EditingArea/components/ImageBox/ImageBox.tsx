a7bd340eb6ea07fe7844fd49ba7ed3942bc87b8e
yui880 <kimyuna521@gmail.com>
2024-07-16 11:32:18 +0900

    fix(ImageBox): 이미지 pointer event 금지해 드래그 오류 수정
    드래그 로직과 이미지 자체를 포인터로 움직이려는 Ponter Event가 같이 실행되어서 이동이 제대로 되지 않는 오류 존재했음. pointer-events-none 처리하여 해결함

--- card-capture/src/components/editor/EditingArea/components/ImageBox/ImageBox.tsx
+++ card-capture/src/components/editor/EditingArea/components/ImageBox/ImageBox.tsx
@@ -9,7 +9,7 @@ type ImageBoxProps = {
 const ImageBox = ({ url, position }: ImageBoxProps) => {
   return (
     <div className="h-full w-full" style={{ opacity: position.opacity }}>
-      <Image src={url} alt="image" width={position.width} height={position.height} />
+      <Image src={url} alt="image" className="pointer-events-none" width={position.width} height={position.height} />
     </div>
   );
 };
