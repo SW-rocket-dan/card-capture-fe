e316a86ac5e607d76ff4a5ec337636529ccf7439
yui880 <kimyuna521@gmail.com>
2024-07-17 14:12:26 +0900

    feat(NavigationBar): 스크롤 위치에 따라 배경이 투명/불투명하게 변경되는 기능 구현

--- card-capture/src/app/page.tsx
+++ card-capture/src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client';
 
+import NavigationBar from '@/components/common/NavigationBar/NavigationBar';
+import React from 'react';
+
 export default function Home() {
   return (
-    <>
-      <div className="bg-blue-400">안뇽</div>
-    </>
+    <div className="w-screen overflow-y-scroll font-Pretendard">
+      <div className="flex h-full flex-row overflow-y-scroll pt-[65px]">
+        <NavigationBar isTransparent={true} />
+      </div>
+    </div>
   );
 }
--- card-capture/src/components/common/NavigationBar/NavigationBar.tsx
+++ card-capture/src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,8 +1,39 @@
 import BeforeLoginNav from '@/components/common/NavigationBar/BeforeLoginNav';
+import { useEffect, useState } from 'react';
 
-const NavigationBar = () => {
+type NavigationBarProps = {
+  isTransparent: boolean;
+};
+
+const NavigationBar = ({ isTransparent = false }: NavigationBarProps) => {
+  const [navBg, setNavBg] = useState('bg-transparent');
+
+  const scrollNavHandler = () => {
+    if (!isTransparent) return;
+
+    if (window.scrollY > 500) {
+      setNavBg('bg-white');
+    } else {
+      setNavBg('bg-transparent');
+    }
+  };
+
+  useEffect(() => {
+    if (isTransparent) {
+      window.addEventListener('scroll', scrollNavHandler);
+    }
+
+    return () => {
+      if (isTransparent) {
+        window.removeEventListener('scroll', scrollNavHandler);
+      }
+    };
+  }, [isTransparent]);
+
   return (
-    <div className="fixed left-0 top-0 flex min-h-[65px] w-full items-center justify-between border-b-[1px] border-border bg-white px-[30px]">
+    <div
+      className={`fixed left-0 top-0 flex min-h-[65px] w-full items-center justify-between border-b-[1px] border-border px-[30px] ${isTransparent ? navBg : 'bg-white'}`}
+    >
       <div className="text-md">Card Capture</div>
       <BeforeLoginNav />
     </div>
