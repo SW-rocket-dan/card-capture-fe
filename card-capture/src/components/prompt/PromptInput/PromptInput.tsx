383f8eb8b6e0aa821f0aa7d4bb9772cb3410aba1
yui880 <kimyuna521@gmail.com>
2024-07-24 16:40:19 +0900

    feat(PromptTextInput): 카드 뉴스 문구 입력 로직 연결

--- card-capture/src/components/prompt/PromptInput/PromptInput.tsx
+++ card-capture/src/components/prompt/PromptInput/PromptInput.tsx
@@ -3,11 +3,32 @@ import PromptPurposeInput from '@/components/prompt/PromptInput/components/Promp
 import PromptColorInput from '@/components/prompt/PromptInput/components/PromptColorInput/PromptColorInput';
 import PromptModelInput from '@/components/prompt/PromptInput/components/PromptModelInput/PromptModelInput';
 import PromptOptionInput from '@/components/prompt/PromptInput/components/PromptOptionInput/PromptOptionInput';
+import { useFieldArray, useForm } from 'react-hook-form';
+
+export type PromptInputFormType = {
+  phrases: { value: string }[];
+  emphasis: { value: string }[];
+  purpose: string;
+  color: string;
+  model: string;
+};
 
 const PromptInput = () => {
+  const { watch, control, register, getValues, setValue } = useForm<PromptInputFormType>({
+    mode: 'onBlur',
+    defaultValues: { phrases: [{ value: '' }] },
+  });
+
+  const phraseFieldArray = useFieldArray({ control, name: 'phrases' });
+  const emphasisFieldArray = useFieldArray({ control, name: 'emphasis' });
+
   return (
     <div className="flex min-w-[300px] max-w-[630px] flex-col gap-[30px] border-b border-border pb-[60px] lg:border-b-0 lg:border-r lg:pr-[100px]">
-      <PromptTextInput />
+      <PromptTextInput
+        register={register}
+        emphasisFieldArray={emphasisFieldArray}
+        phraseFieldArray={phraseFieldArray}
+      />
       <PromptPurposeInput />
       <PromptColorInput />
       <PromptModelInput />
