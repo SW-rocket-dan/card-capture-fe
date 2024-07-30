1b1ef19e7d2747974a27ed72c6d54796b76943c4
yui880 <kimyuna521@gmail.com>
2024-07-30 11:02:11 +0900

    feat(CardArea): 이미지 export 시 progress 보이게 추가
    shadcn.ui의 progress 사용

--- card-capture/src/components/editor/EditingArea/views/CardArea.tsx
+++ card-capture/src/components/editor/EditingArea/views/CardArea.tsx
@@ -8,13 +8,14 @@ import LayerAddBox from '@/components/editor/EditingArea/views/LayerAddBox';
 import Button from '@/components/common/Button/Button';
 import { useFocusStore } from '@/store/useFocusStore';
 import ImageBox from '@/components/editor/EditingArea/components/ImageBox/ImageBox';
-import { Dialog, DialogContent, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import React, { useRef, useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useCardsStore } from '@/store/useCardsStore';
 import { parseEscapedJson } from '@/utils/jsonUtils';
 import html2canvas from 'html2canvas';
 import { saveAs } from 'file-saver';
-import { toBlob, toCanvas, toPng } from 'html-to-image';
+import { toPng } from 'html-to-image';
+import { Progress } from '@/components/ui/progress';
 
 const CardArea = ({ card }: { card: Card }) => {
   const cardId = card.id;
@@ -44,7 +45,6 @@ const CardArea = ({ card }: { card: Card }) => {
 
   const changeCardHandler = () => {
     const templateData = parseEscapedJson(json);
-
     setCard(templateData.cards);
 
     setIsOpen(false);
@@ -54,33 +54,24 @@ const CardArea = ({ card }: { card: Card }) => {
    * card에 그려진 dom을 image export 하는 handler
    * html2canvas와 file-saver 사용
    */
+  const [isDownloading, setIsDownloading] = useState(false);
+  const [progress, setProgress] = useState(10);
   const cardRef = useRef<HTMLDivElement>(null);
 
   const downloadCardHandler = async () => {
     if (!cardRef || !cardRef.current) return;
 
+    // 포커스 된 항목이 있는 경우 FocusBox의 테두리도 함께 출력될 수 있으므로 제거
     setFocusedLayerId(-1);
     await new Promise(resolve => setTimeout(resolve, 100));
+
+    // 다운로드가 시작되면서 대기 모달이 떠야 함으로 상태 변경
+    setIsDownloading(true);
 
     try {
       const cardArea = cardRef.current;
 
-      // const canvas = await html2canvas(cardArea, {
-      //   scale: 4,
-      //   scrollX: 0,
-      //   scrollY: 0,
-      //   width: cardArea.offsetWidth,
-      //   height: cardArea.offsetHeight,
-      //   windowWidth: document.documentElement.scrollWidth,
-      //   windowHeight: document.documentElement.scrollHeight,
-      //   allowTaint: true,
-      //   useCORS: true,
-      // });
-
-      // canvas.toBlob(blob => {
-      //   if (blob !== null) saveAs(blob, 'card-capture-image.png');
-      // });
-
+      // html-to-image 사용하여 이미지로 변경
       const dataUrl = await toPng(cardArea, {
         includeQueryParams: true,
         quality: 0.8, // 품질 설정
@@ -88,11 +79,37 @@ const CardArea = ({ card }: { card: Card }) => {
         height: cardArea.offsetHeight,
       });
 
+      // file-saver 라이브러리 사용하여 자동 다운로드
       saveAs(dataUrl, 'card-capture-image.png');
     } catch (e) {
       console.error('Error converting card to image', e);
+    } finally {
+      setIsDownloading(false);
+      setProgress(10);
     }
   };
+
+  /**
+   * progress 바를 10씩 증가시키는 함수
+   */
+  const increaseProgress = useCallback(() => {
+    if (progress < 90) {
+      setProgress(prevProgress => prevProgress + 10);
+    }
+  }, [progress]);
+
+  /**
+   * 3초에 한번씩 progress를 증가시키는 로직
+   */
+  useEffect(() => {
+    let timer: NodeJS.Timeout | undefined;
+    if (isDownloading) {
+      timer = setInterval(increaseProgress, 300);
+    }
+    return () => {
+      if (timer) clearInterval(timer);
+    };
+  }, [isDownloading, increaseProgress]);
 
   return (
     <div className="flex flex-1 flex-col items-center justify-center gap-[10px] bg-editorbg">
@@ -125,6 +142,20 @@ const CardArea = ({ card }: { card: Card }) => {
           </Button>
         </div>
       </div>
+
+      {/* export 중에 뜨는 모달 */}
+      <Dialog open={isDownloading} onOpenChange={setIsDownloading}>
+        <DialogContent className="flex w-[400px] flex-col items-center justify-center px-[20px] py-[40px]">
+          <div className="flex flex-col items-center justify-center gap-1">
+            <DialogTitle>카드뉴스를 이미지로 변환하고 있어요!</DialogTitle>
+            <DialogDescription>대기 창을 닫아도 다운로드는 문제없이 진행됩니다</DialogDescription>
+          </div>
+          <div className="flex flex-col">
+            <Progress value={progress} className="w-[250px]" />
+            <div className="h-3 w-3 animate-spin" />
+          </div>
+        </DialogContent>
+      </Dialog>
 
       {/* card에 요소 출력 */}
       <div
