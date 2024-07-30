a2229be94512eb2e941f31b66ce8302f4eb67ebc
yui880 <kimyuna521@gmail.com>
2024-07-30 13:45:26 +0900

    fix(OpacityButton): input에 handler 없는 오류 수정

--- card-capture/src/components/editor/Tab/components/EditTab/common/OpacityButton.tsx
+++ card-capture/src/components/editor/Tab/components/EditTab/common/OpacityButton.tsx
@@ -1,6 +1,6 @@
 import { GraySlider } from '@/components/ui/graySlider';
 import useLayerStyles from '@/components/editor/Tab/hooks/useLayerStyles';
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { ChangeEvent, Dispatch, SetStateAction, useEffect } from 'react';
 
 type OpacityButtonProps = {
   opacity: number;
@@ -21,12 +21,26 @@ const OpacityButton = ({ opacity, setOpacity }: OpacityButtonProps) => {
     changePositionHandler({ opacity: value[0] });
   };
 
+  const changeOpacityInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value);
+
+    setOpacity(value);
+    changePositionHandler({ opacity: value });
+  };
+
   return (
     <div className="flex flex-col gap-2.5 rounded-[10px] border-[1px] border-border px-[10px] py-[10px]">
       <div className="flex flex-row items-center justify-between">
         <p className="text-xs text-gray4">투명도</p>
         <div className="flex w-[80px] flex-row justify-between rounded-md bg-itembg px-3 py-[5px] text-xs">
-          <input type="number" value={opacity} min={0} max={100} className="bg-transparent font-medium outline-none" />
+          <input
+            type="number"
+            onChange={changeOpacityInputHandler}
+            value={opacity}
+            min={0}
+            max={100}
+            className="bg-transparent font-medium outline-none"
+          />
           <span>%</span>
         </div>
       </div>
