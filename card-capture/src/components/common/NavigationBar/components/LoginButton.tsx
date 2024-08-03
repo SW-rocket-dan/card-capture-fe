b986ebd63ae0cbc1fcccc935245e8e32a7e5303b
yui880 <kimyuna521@gmail.com>
2024-08-04 03:09:54 +0900

    refactor(CreateButton): 로그인 버튼들 반응형 수정

--- card-capture/src/components/common/NavigationBar/components/LoginButton.tsx
+++ card-capture/src/components/common/NavigationBar/components/LoginButton.tsx
@@ -11,6 +11,7 @@ import DivButton from '@/components/common/Button/DivButton';
 import useGoogleAuth from '@/hooks/useGoogleAuth';
 import React, { useEffect, useState } from 'react';
 import { useAuthStore } from '@/store/useAuthStore';
+import useIsMobile from '@/hooks/useIsMobile';
 
 const LoginButton = () => {
   const { isLoggedIn, loginHandler } = useGoogleAuth();
@@ -22,6 +23,8 @@ const LoginButton = () => {
     setIsOpen(isModalOpen);
   }, [isModalOpen]);
 
+  const { isMobile } = useIsMobile();
+
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
       <DialogTrigger>
@@ -31,29 +34,22 @@ const LoginButton = () => {
         </DivButton>
       </DialogTrigger>
 
-      <DialogContent className="h-[270px] w-[400px] px-7 py-10">
-        {!isLoggedIn ? (
-          <DialogHeader>
-            <DialogTitle className="text-center">
-              지금 로그인하고
-              <br /> 3분만에 카드뉴스를 만들어보세요!
-            </DialogTitle>
-            <DialogDescription>로그인하면 Card Capture와 함께 AI 카드 포스터를 만들 수 있어요!</DialogDescription>
+      <DialogContent className="h-[270px] w-[300px] px-7 py-10 sm:w-[400px]">
+        <DialogHeader>
+          <DialogTitle className="text-center">
+            지금 로그인하고
+            <br /> 3분만에 카드뉴스를 만들어보세요!
+          </DialogTitle>
+          <DialogDescription>
+            로그인하면 Card Capture와 함께 {isMobile && <br />} AI 카드 포스터를 만들 수 있어요!
+          </DialogDescription>
 
-            <div className="flex flex-col py-5">
-              <button onClick={loginHandler}>
-                <img alt="google-login" src="/image/web_light_sq_SI.svg" className="w-[200px]" />
-              </button>
-            </div>
-          </DialogHeader>
-        ) : (
-          <DialogHeader>
-            <DialogTitle className="text-center">로그인 성공!</DialogTitle>
-            <DialogDescription>Card Capture와 함께 AI 카드 포스터를 만들어보세요</DialogDescription>
-
-            <img alt="cat" src="/image/cat.png" className="h-[150px] w-[180px] object-cover" />
-          </DialogHeader>
-        )}
+          <div className="flex flex-col py-5">
+            <button onClick={loginHandler}>
+              <img alt="google-login" src="/image/web_light_sq_SI.svg" className="w-[200px]" />
+            </button>
+          </div>
+        </DialogHeader>
       </DialogContent>
     </Dialog>
   );
