cc26f2e1a33a6d1ea24a47a12389abd86916003a
yui880 <kimyuna521@gmail.com>
2024-08-05 17:02:31 +0900

    feat(AuthCheck): 토큰 유무 확인하여 자동 로그인 적용

--- card-capture/src/app/layout.tsx
+++ card-capture/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import AuthCheck from '@/components/common/Auth/AuthCheck';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AuthCheck />
+        {children}
+      </body>
     </html>
   );
 }
