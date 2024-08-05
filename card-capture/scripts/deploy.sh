d0a63e859697ac8e246261ce6cc4f068aab97809
inpink <108166692+inpink@users.noreply.github.com>
2024-08-05 21:05:26 +0900

    Update deploy.sh

--- card-capture/scripts/deploy.sh
+++ card-capture/scripts/deploy.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# 기존 폴더가 있으면 삭제 (루트 권한 사용)
-if [ -d "/home/ec2-user/card-capture-fe" ]; then
-    sudo rm -rf /home/ec2-user/card-capture-fe
-fi
-
-# 새로운 폴더 생성
-mkdir -p /home/ec2-user/card-capture-fe
 
 cd /home/ec2-user/card-capture-fe
 
