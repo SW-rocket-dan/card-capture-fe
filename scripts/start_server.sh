993f8d57a29ca20a746c0af73380809cd3e7fe50
inpink <108166692+inpink@users.noreply.github.com>
2024-08-05 19:14:41 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-cd /home/ec2-user/app
-npm start
+
+cd /home/ec2-user/card-capture-fe
+
+# PM2 프로세스 삭제
+pm2 delete card-capture || true
+
+# PM2로 앱 시작
+NODE_OPTIONS=--max-old-space-size=8192 pm2 start npm --name "card-capture" -- start
+
+# PM2 설정 저장
+pm2 save
+
+# PM2 부팅 시 시작 설정
+pm2 startup | tail -n 1 | sudo bash
