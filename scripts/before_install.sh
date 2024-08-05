e4fa60aa2fa21eb0782e85284a5133c192a4a532
inpink <108166692+inpink@users.noreply.github.com>
2024-08-05 19:14:25 +0900

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# 기존 폴더가 있으면 삭제
-if [ -d "/home/ec2-user/app" ]; then
-    rm -rf /home/ec2-user/app
-fi
+cd /home/ec2-user/card-capture-fe
+# npm install
+npm install
 
-# 새로운 폴더 생성
-mkdir -p /home/ec2-user/app
+# 환경 변수 설정
+echo "NEXT_PUBLIC_API_KEY=${NEXT_PUBLIC_API_KEY}" > .env
+
+# 빌드
+NODE_OPTIONS=--max-old-space-size=8192 npm run build
