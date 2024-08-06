d24628e5a010fb764f717caa0f175840e98d38da
yui880 <kimyuna521@gmail.com>
2024-08-06 16:58:16 +0900

    feat(ReactQueryProvider): app router에 리액트 쿼리 적용하기 위한 provider 구현 및 적용

--- card-capture/src/app/layout.tsx
+++ card-capture/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthCheck from '@/components/common/Auth/AuthCheck';
+import ReactQueryProvider from '@/utils/provider/ReactQueryProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthCheck />
-        {children}
+        <ReactQueryProvider>
+          <AuthCheck />
+          {children}
+        </ReactQueryProvider>
       </body>
     </html>
   );
