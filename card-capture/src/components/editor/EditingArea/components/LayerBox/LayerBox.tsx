def5d0263f415bd51ea6fdddbc3585ea73dd40fd
yui880 <kimyuna521@gmail.com>
2024-08-16 11:03:21 +0900

    refactor(LayerBox): hover시 border의 z-index 증가하도록 변경
    border가 가려져서 안보이는 현상을 해결하기 위함

--- card-capture/src/components/editor/EditingArea/components/LayerBox/LayerBox.tsx
+++ card-capture/src/components/editor/EditingArea/components/LayerBox/LayerBox.tsx
@@ -1,41 +1,52 @@
-//PositionBox.tsx
-
+import React from 'react';
 import { Position } from '@/store/useCardsStore/type';
 
-type Props = {
+type LayerBoxProps = {
   children: React.ReactNode;
   position: Position;
   onClick: (e: React.MouseEvent) => void;
 };
 
 /**
- *
- * @param component Box안에 띄어줄 컴포넌트
+ * @param children Box안에 띄어줄 컴포넌트
  * @param position 위치정보에 따라서 위치를 렌더링해줌
  * @param onClick 클릭시 focusBox로 변경
- * @returns
  */
-const LayerBox = ({ children, position, onClick }: Props) => {
-  //고민해봐야할 부분 동적 스타일링은 tailwind로 쉽지않음
-  //style로 바로 박을것인지, tailwind로 다 정의해 둘것인지, 혹은 css로할 것인지
+const LayerBox = ({ children, position, onClick }: LayerBoxProps) => {
+  const commonStyles = {
+    left: position.x,
+    top: position.y,
+    width: position.width,
+    height: position.height,
+    transform: `rotate(${position.rotate}deg)`,
+    transformOrigin: 'center',
+  };
+
   return (
-    <div
-      className="absolute border-2 border-transparent px-3 py-1.5 hover:border-main"
-      style={{
-        left: position.x,
-        top: position.y,
-        width: position.width,
-        height: position.height,
-        zIndex: position.zIndex,
-        opacity: position.opacity / 100,
-        transform: `rotate(${position.rotate}deg)`,
-        transformOrigin: 'center',
-        boxSizing: 'border-box',
-      }}
-      onMouseDown={onClick}
-    >
-      <div className="absolute inset-0 flex items-center justify-start p-3">{children}</div>
-    </div>
+    <>
+      {/* Border layer */}
+      <div
+        className="absolute border-2 border-transparent hover:border-main"
+        style={{
+          ...commonStyles,
+          zIndex: 1000,
+        }}
+        onMouseDown={onClick}
+      />
+
+      {/* Content layer */}
+      <div
+        className="absolute"
+        style={{
+          ...commonStyles,
+          zIndex: position.zIndex,
+          opacity: position.opacity / 100,
+          boxSizing: 'border-box',
+        }}
+      >
+        <div className="absolute inset-0 flex items-center justify-start p-3">{children}</div>
+      </div>
+    </>
   );
 };
 
