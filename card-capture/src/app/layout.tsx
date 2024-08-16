871d4e40d716a6ad0e9cc4f435533286b7463781
yui880 <kimyuna521@gmail.com>
2024-08-16 16:58:25 +0900

    feat(TextFont): 텍스트 에디터에 구글 폰트 추가

--- card-capture/src/app/layout.tsx
+++ card-capture/src/app/layout.tsx
@@ -5,6 +5,7 @@ import AuthCheck from '@/components/common/Auth/AuthCheck';
 import ReactQueryProvider from '@/utils/provider/ReactQueryProvider';
 import { Toaster } from '@/components/ui/toaster';
 import AmplitudeContextProvider from '@/utils/provider/AmplitudeContextProvider';
+import fonts from '@/components/editor/EditingArea/components/TextBox/TextFonts';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,8 +22,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const fontVariables = Object.values(fonts)
+    .map(font => font.variable)
+    .join(' ');
+
   return (
-    <html lang="en">
+    <html lang="en" className={fontVariables}>
       <body className={inter.className}>
         <AmplitudeContextProvider>
           <ReactQueryProvider>
