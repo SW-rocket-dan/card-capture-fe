ba41a18bb96dc3af2b7889364fe9626c01bdd7f4
yui880 <kimyuna521@gmail.com>
2024-08-19 17:03:50 +0900

    fix(BackgroundEditBox): useEffect로 인한 maximum call stack 에러 해결
    리렌더링으로 인한 무한 루프 상태에 걸릴 위험이 있어서 useEffect가 아닌 event handler로 값을 업데이트 하도록 변경함

--- card-capture/src/components/editor/Tab/components/EditTab/BackgroundEditBox/BackgroundEditBox.tsx
+++ card-capture/src/components/editor/Tab/components/EditTab/BackgroundEditBox/BackgroundEditBox.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import UpIcon from '@/components/common/Icon/UpIcon';
 import DownIcon from '@/components/common/Icon/DownIcon';
 import OpacityButton from '@/components/editor/Tab/components/EditTab/common/OpacityButton';
 import ImageButton from '@/components/editor/Tab/components/EditTab/common/ImageButton';
 import ColorButton from '@/components/editor/Tab/components/EditTab/common/ColorButton';
-import { useColor } from 'react-color-palette';
+import { IColor, useColor } from 'react-color-palette';
 import { useCardsStore } from '@/store/useCardsStore';
 import { useFocusStore } from '@/store/useFocusStore';
 import useImageUploader from '@/hooks/useImageUploader';
@@ -18,13 +18,19 @@ const BackgroundEditBox = ({ focused = false }: { focused?: boolean }) => {
    * 배경 색상과 투명도가 변경되면 store에 업데이트 하는 로직
    */
   const [color, setColor] = useColor(background?.color || '#FFFFFF');
-  const [opacity, setOpacity] = useState<number>(100);
+  const [opacity, setOpacity] = useState<number>(background?.opacity || 100);
 
   const setBackground = useCardsStore(state => state.setBackground);
 
-  useEffect(() => {
-    setBackground(focusedCardId, { color: color.hex, opacity: opacity });
-  }, [color, opacity]);
+  const changeColorHandler = (newColor: IColor) => {
+    setColor(newColor);
+    setBackground(focusedCardId, { color: newColor.hex, opacity });
+  };
+
+  const changeOpacityHandler = (newOpacity: number) => {
+    setOpacity(newOpacity);
+    setBackground(focusedCardId, { color: color.hex, opacity: newOpacity });
+  };
 
   /**
    * 드롭다운 열고 닫는 로직
@@ -46,7 +52,12 @@ const BackgroundEditBox = ({ focused = false }: { focused?: boolean }) => {
       >
         <p>배경</p>
         <div className="flex flex-row gap-3">
-          <ColorButton color={color} setColor={setColor} className="h-4 w-20 rounded-[5px]" direction="right" />
+          <ColorButton
+            color={color}
+            setColor={changeColorHandler}
+            className="h-4 w-20 rounded-[5px]"
+            direction="right"
+          />
           {isOpen ? <UpIcon width={13} className="text-gray1" /> : <DownIcon width={13} className="text-gray1" />}
         </div>
       </div>
@@ -55,7 +66,7 @@ const BackgroundEditBox = ({ focused = false }: { focused?: boolean }) => {
         <div className="flex flex-col gap-[12px] px-[15px] pb-[20px]">
           {background?.url === '' ? <ImageButton onChangeImage={addBackgroundImageHandler} /> : <BackgroundDeleteBox />}
 
-          <OpacityButton opacity={opacity} setOpacity={setOpacity} />
+          <OpacityButton opacity={opacity} setOpacity={changeOpacityHandler} />
         </div>
       )}
     </div>
