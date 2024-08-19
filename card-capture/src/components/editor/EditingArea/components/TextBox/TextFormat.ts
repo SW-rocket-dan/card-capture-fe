61986351f7f22c1a0eb31fd7b8acec9e602dd383
yui880 <kimyuna521@gmail.com>
2024-08-19 16:31:36 +0900

    feat(TextFormat): 텍스트 크기 추가 등록
    등록한 크기로만 텍스트를 변경할 수 있는데 서버에서 등록한 숫자 이외의 값이 올 수도 있어서 해당 문제를 해결하기 위해 1~128 사이의 모든 숫자를 일단 등록해둠

--- card-capture/src/components/editor/EditingArea/components/TextBox/TextFormat.ts
+++ card-capture/src/components/editor/EditingArea/components/TextBox/TextFormat.ts
@@ -40,9 +40,10 @@ Quill.register(Font, true);
 // 폰트 사이즈
 
 export const availableFontSize = ['12px', '16px', '18px', '24px', '28px', '32px', '48px', '64px', '96px', '128px'];
+const fontSizeList = Array.from({ length: 128 }, (_, i) => `${i + 1}px`);
 
 let Size = Quill.import('attributors/style/size');
-Size.whitelist = availableFontSize;
+Size.whitelist = fontSizeList;
 Quill.register(Size, true);
 
 // 자간 (Letter Spacing)
