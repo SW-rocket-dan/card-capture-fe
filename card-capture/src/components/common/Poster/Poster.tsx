46832365c92143cbcc4bfc65f5560af3f80d396c
yui880 <kimyuna521@gmail.com>
2024-09-10 21:54:48 +0900

    feat(Poster): 템플릿을 렌더링하는 용도의 Poster 컴포넌트 구현

---
+++ card-capture/src/components/common/Poster/Poster.tsx
@@ -0,0 +1,78 @@
+import { Card, Illust, Image, Shape, Text } from '@/store/useCardsStore/type';
+import ShapeBox from '@/components/editor/EditingArea/components/ShapeBox/ShapeBox';
+import ImageBox from '@/components/editor/EditingArea/components/ImageBox/ImageBox';
+import IllustBox from '@/components/editor/EditingArea/components/IllustBox/IllustBox';
+import React from 'react';
+import RenderBox from '@/components/common/Poster/components/RenderBox';
+import ReadOnlyTextBox from '@/components/common/Poster/components/ReadOnlyTextBox';
+
+type TemplateProps = {
+  size: number;
+  card: Card | undefined;
+};
+
+const Poster = ({ size, card }: TemplateProps) => {
+  if (!card) return <div></div>;
+
+  const cardId = card.id;
+  const background = card.background;
+
+  const scaleRatio = size / 550;
+
+  return (
+    <div
+      className={`relative overflow-hidden border-[1px] border-border bg-white`}
+      style={{
+        height: `${size}px`,
+        width: `${size}px`,
+        userSelect: 'auto',
+        backgroundImage: `url(${background.url})`,
+        backgroundSize: 'contain',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor: background.color,
+        opacity: background.opacity / 100,
+      }}
+    >
+      {card.layers.map((layer, idx) => {
+        if (layer.type === 'text') {
+          const contentText = layer.content as Text;
+
+          // Text Layer Box
+          return (
+            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+              <ReadOnlyTextBox text={contentText.content} />
+              <div></div>
+            </RenderBox>
+          );
+        } else if (layer.type === 'shape') {
+          // 도형 Layer Box
+          const { type, color } = layer.content as Shape;
+          return (
+            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+              <ShapeBox shapeType={type} color={color} />
+            </RenderBox>
+          );
+        } else if (layer.type === 'image') {
+          const { url } = layer.content as Image;
+
+          return (
+            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+              <ImageBox url={url} position={layer.position} />
+            </RenderBox>
+          );
+        } else if (layer.type === 'illust') {
+          const { url } = layer.content as Illust;
+
+          return (
+            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+              <IllustBox url={url} position={layer.position} />
+            </RenderBox>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default Poster;
