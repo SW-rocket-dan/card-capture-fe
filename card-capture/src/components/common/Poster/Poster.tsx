090708ae0931186fc6cb500262b72fca1fe05365
yui880 <kimyuna521@gmail.com>
2024-09-11 20:08:15 +0900

    feat(Poster): ref 하위 컴포넌트 전달 위해 forwardRef 적용

--- card-capture/src/components/common/Poster/Poster.tsx
+++ card-capture/src/components/common/Poster/Poster.tsx
@@ -2,79 +2,80 @@ import { Card, Illust, Image, Shape, Text } from '@/store/useCardsStore/type';
 import ShapeBox from '@/components/editor/EditingArea/components/ShapeBox/ShapeBox';
 import ImageBox from '@/components/editor/EditingArea/components/ImageBox/ImageBox';
 import IllustBox from '@/components/editor/EditingArea/components/IllustBox/IllustBox';
-import React from 'react';
+import React, { forwardRef } from 'react';
 import RenderBox from '@/components/common/Poster/components/RenderBox';
 import ReadOnlyTextBox from '@/components/common/Poster/components/ReadOnlyTextBox';
 
-type TemplateProps = {
+type PosterProps = {
   size: number;
   card: Card | undefined;
   isSelected?: boolean;
 };
 
-const Poster = ({ size, card, isSelected }: TemplateProps) => {
+const Poster = forwardRef<HTMLDivElement, PosterProps>(({ size, card, isSelected }, ref) => {
   if (!card) return <div></div>;
 
-  const cardId = card.id;
   const background = card.background;
-
   const scaleRatio = size / 550;
 
   return (
-    <div
-      className={`relative overflow-hidden border-[2.5px] bg-white ${isSelected ? 'border-main' : 'border-transparent'}`}
-      style={{
-        height: `${size}px`,
-        width: `${size}px`,
-        userSelect: 'auto',
-        backgroundImage: `url(${background.url})`,
-        backgroundSize: 'contain',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor: background.color,
-        opacity: background.opacity / 100,
-        boxSizing: 'border-box',
-      }}
-    >
-      {card.layers.map((layer, idx) => {
-        if (layer.type === 'text') {
-          const contentText = layer.content as Text;
+    <div className={`border-[2.5px] ${isSelected ? 'border-main' : 'border-transparent'}`}>
+      <div
+        ref={ref}
+        className={`relative overflow-hidden bg-white`}
+        style={{
+          height: `${size}px`,
+          width: `${size}px`,
+          userSelect: 'auto',
+          backgroundImage: `url(${background.url})`,
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: background.color,
+          opacity: background.opacity / 100,
+          boxSizing: 'border-box',
+        }}
+      >
+        {card.layers.map((layer, idx) => {
+          if (layer.type === 'text') {
+            const contentText = layer.content as Text;
 
-          // Text Layer Box
-          return (
-            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
-              <ReadOnlyTextBox text={contentText.content} />
-              <div></div>
-            </RenderBox>
-          );
-        } else if (layer.type === 'shape') {
-          // 도형 Layer Box
-          const { type, color } = layer.content as Shape;
-          return (
-            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
-              <ShapeBox shapeType={type} color={color} />
-            </RenderBox>
-          );
-        } else if (layer.type === 'image') {
-          const { url } = layer.content as Image;
+            // Text Layer Box
+            return (
+              <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+                <ReadOnlyTextBox text={contentText.content} />
+                <div></div>
+              </RenderBox>
+            );
+          } else if (layer.type === 'shape') {
+            // 도형 Layer Box
+            const { type, color } = layer.content as Shape;
+            return (
+              <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+                <ShapeBox shapeType={type} color={color} />
+              </RenderBox>
+            );
+          } else if (layer.type === 'image') {
+            const { url } = layer.content as Image;
 
-          return (
-            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
-              <ImageBox url={url} position={layer.position} />
-            </RenderBox>
-          );
-        } else if (layer.type === 'illust') {
-          const { url } = layer.content as Illust;
+            return (
+              <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+                <ImageBox url={url} position={layer.position} />
+              </RenderBox>
+            );
+          } else if (layer.type === 'illust') {
+            const { url } = layer.content as Illust;
 
-          return (
-            <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
-              <IllustBox url={url} position={layer.position} />
-            </RenderBox>
-          );
-        }
-      })}
+            return (
+              <RenderBox key={idx} position={layer.position} ratio={scaleRatio}>
+                <IllustBox url={url} position={layer.position} />
+              </RenderBox>
+            );
+          }
+        })}
+      </div>
     </div>
   );
-};
+});
 
 export default Poster;
--- card-capture/src/components/prompt/result/ResultTemplate/components/SelectedPoster.tsx
+++ card-capture/src/components/prompt/result/ResultTemplate/components/SelectedPoster.tsx
@@ -1,6 +1,7 @@
 import CheckBox from '@/components/common/CheckBox/CheckBox';
 import Poster from '@/components/common/Poster/Poster';
 import { Card } from '@/store/useCardsStore/type';
+import { forwardRef } from 'react';
 
 type SelectedPosterProps = {
   size: number;
@@ -9,23 +10,25 @@ type SelectedPosterProps = {
   setIsChecked: () => void;
 };
 
-const SelectedPoster = ({ size, card, isChecked, setIsChecked }: SelectedPosterProps) => {
-  return (
-    <div className="flex flex-col gap-[15px]">
-      <div className="flex flex-row items-center gap-[13px]">
-        <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} />
-        <div
-          className="rounded-[3px] px-[8px] py-[1px]"
-          style={{ background: 'linear-gradient(90deg, #6F6CFF 0%, rgba(255, 126, 85, 0.88) 100%)' }}
-        >
-          <span className="text-[13px] font-semibold text-white">Premium</span>
+const SelectedPoster = forwardRef<HTMLDivElement, SelectedPosterProps>(
+  ({ size, card, isChecked, setIsChecked }, ref) => {
+    return (
+      <div className="flex flex-col gap-[15px]">
+        <div className="flex flex-row items-center gap-[13px]">
+          <CheckBox isChecked={isChecked} setIsChecked={setIsChecked} />
+          <div
+            className="rounded-[3px] px-[8px] py-[1px]"
+            style={{ background: 'linear-gradient(90deg, #6F6CFF 0%, rgba(255, 126, 85, 0.88) 100%)' }}
+          >
+            <span className="text-[13px] font-semibold text-white">Premium</span>
+          </div>
         </div>
+        <button onClick={setIsChecked}>
+          <Poster ref={ref} size={size} card={card} isSelected={isChecked} />
+        </button>
       </div>
-      <button onClick={setIsChecked}>
-        <Poster size={size} card={card} isSelected={isChecked} />
-      </button>
-    </div>
-  );
-};
+    );
+  },
+);
 
 export default SelectedPoster;
