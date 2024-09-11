65ed91dc01c99a1a71aea208f015396327e51294
yui880 <kimyuna521@gmail.com>
2024-09-11 12:25:38 +0900

    feat(CheckBox): 체크박스 공통 컴포넌트 구현

---
+++ card-capture/src/components/common/CheckBox/CheckBox.tsx
@@ -0,0 +1,20 @@
+import CheckIcon from '@/components/common/Icon/CheckIcon';
+import * as React from 'react';
+
+type CheckBoxProps = {
+  isChecked: boolean;
+  setIsChecked: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const CheckBox = ({ isChecked, setIsChecked }: CheckBoxProps) => {
+  return (
+    <div
+      className={`flex h-[20px] w-[20px] cursor-pointer items-center justify-center rounded-[4px] border-[1.5px] ${isChecked ? 'border-main' : 'border-border'}`}
+      onClick={() => setIsChecked(prev => !prev)}
+    >
+      <CheckIcon width={15} className={`stroke-[1px] ${isChecked ? 'text-main' : 'text-border'}`} />
+    </div>
+  );
+};
+
+export default CheckBox;
