7d795d890ab2bb6bde5a244c1ce69712b31f0702
yui880 <kimyuna521@gmail.com>
2024-09-20 17:55:30 +0900

    feat(PromptHistoryBox): 이전 프롬프트 데이터 출력
    QueryClient 사용하여 캐싱된 데이터 출력

--- card-capture/src/components/editor/Tab/components/PromptTab/PromptHistoryBox/PromptHistoryBox.tsx
+++ card-capture/src/components/editor/Tab/components/PromptTab/PromptHistoryBox/PromptHistoryBox.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import { QueryClient, useQueryClient } from '@tanstack/react-query';
+import { useParams } from 'next/navigation';
+import { Template } from '@/types';
 
 const PromptHistoryBox = () => {
+  /**
+   * 라우팅된 아이디 기반으로 캐시에서 템플릿 데이터 가져오기
+   */
+  const params = useParams();
+  const id = params.templateId as string;
+
+  const queryClient = useQueryClient();
+  const templateData = queryClient.getQueryData<Template>([`template-${id}`]);
+
   return (
     <div className="flex w-full flex-col">
       <p
@@ -12,17 +24,21 @@ const PromptHistoryBox = () => {
       <div className="flex flex-col gap-4 pb-[15px]">
         <div className="flex flex-col gap-2 px-[15px]">
           <label className="text-[12px] text-gray4">입력한 문구</label>
-          <p className="h-[90px] rounded-[10px] border border-border p-1"></p>
+          <p className="text-gray9 h-[90px] overflow-y-scroll rounded-[10px] border border-border p-2.5 text-[12px]">
+            {templateData?.prompt.phraseDetails.phrases.map(str => <p>{str}</p>)}
+          </p>
         </div>
         <div className="flex flex-col gap-2 px-[15px]">
           <label className="text-[12px] text-gray4">목적</label>
-          <p className="rounded-[10px] border border-border p-1"></p>
+          <p className="text-gray9 rounded-[10px] border border-border p-2.5 text-[12px]">
+            {templateData?.prompt.purpose}
+          </p>
         </div>
         <div className="flex flex-row justify-between px-[15px]">
           <label className="text-[12px] text-gray4">색상</label>
           <div
             className="!h-[19px] !w-[35px] rounded-md border-2 border-border"
-            style={{ backgroundColor: '#111111' }}
+            style={{ backgroundColor: `${templateData?.prompt.color}` }}
           />
         </div>
       </div>
