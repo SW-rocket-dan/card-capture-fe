15d2b621b63be59e09b78c0ffaa3459b1ad46c48
yui880 <kimyuna521@gmail.com>
2024-09-30 14:50:38 +0900

    feat(useTextStyle): 현재 선택된 텍스트의 스타일을 가져오는 hook 구현

--- card-capture/src/components/editor/Tab/components/EditTab/TextEditBox/hooks/useTextStyle.tsx
+++ card-capture/src/components/editor/Tab/components/EditTab/TextEditBox/hooks/useTextStyle.tsx
@@ -1,21 +1,53 @@
 import { useFocusStore } from '@/store/useFocusStore';
+import { useEffect, useState } from 'react';
+
+export type TextStyle = { [key: string]: string | boolean };
 
 const useTextStyle = () => {
   const currentRef = useFocusStore(state => state.currentRef);
+  const [textStyle, setTextStyle] = useState<TextStyle | undefined>({});
 
-  const getTextStyle = () => {
+  /**
+   * 스타일 추출하는 함수
+   */
+  const getTextStyle = (): TextStyle | undefined => {
     if (!currentRef || !currentRef.current) return;
 
     const editor = currentRef.current.getEditor();
-    const currentFormat = editor.getFormat();
-    const range = editor.getSelection();
-
-    if (range && range.length > 0) {
-      console.log(currentFormat);
-    } else console.log(currentFormat);
+    return editor.getFormat();
   };
 
-  return { getTextStyle };
+  /**
+   * 에디터의 이벤트를 모니터링 하여 에디터가 변경되었을 경우 스타일을 업데이트 하는 기능
+   */
+  useEffect(() => {
+    if (!currentRef?.current) return;
+
+    const editor = currentRef.current.getEditor();
+
+    const updateTextStyle = () => {
+      const newStyle = getTextStyle();
+
+      setTextStyle(prevStyle => {
+        if (JSON.stringify(prevStyle) !== JSON.stringify(newStyle)) {
+          return newStyle;
+        }
+        return prevStyle;
+      });
+    };
+
+    editor.on('text-change', updateTextStyle);
+    editor.on('selection-change', updateTextStyle);
+
+    updateTextStyle();
+
+    return () => {
+      editor.off('text-change', updateTextStyle);
+      editor.off('selection-change', updateTextStyle);
+    };
+  }, [currentRef, getTextStyle]);
+
+  return { textStyle };
 };
 
 export default useTextStyle;
