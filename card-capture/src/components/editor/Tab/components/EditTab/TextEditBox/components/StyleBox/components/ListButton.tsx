aa753ccbe172f85b45c984c7adf2a99049985898
yui880 <kimyuna521@gmail.com>
2024-10-02 11:38:12 +0900

    feat(ListButton): 불렛 감지해서 탭에 반영하는 기능 구현

--- card-capture/src/components/editor/Tab/components/EditTab/TextEditBox/components/StyleBox/components/ListButton.tsx
+++ card-capture/src/components/editor/Tab/components/EditTab/TextEditBox/components/StyleBox/components/ListButton.tsx
@@ -1,15 +1,25 @@
 import ListIcon from '@/components/common/Icon/ListIcon';
 import useTextFormatting from '@/components/editor/Tab/components/EditTab/TextEditBox/hooks/useTextFormatting';
+import useTextStyle from '@/components/editor/Tab/components/EditTab/TextEditBox/hooks/useTextStyle';
+import { useEffect, useState } from 'react';
 
 const ListButton = () => {
   const { changeTextFormatHandler } = useTextFormatting();
+  const [isActive, setIsActive] = useState(false);
+
+  const { textStyle, getStyles } = useTextStyle();
+
+  useEffect(() => {
+    const listStyle = getStyles('list');
+    setIsActive(listStyle === 'bullet');
+  }, [textStyle, getStyles]);
 
   return (
     <button
       onClick={() => changeTextFormatHandler('list')}
-      className="flex h-[30px] w-[30px] items-center justify-center rounded-md hover:bg-itembg"
+      className={`flex h-[30px] w-[30px] items-center justify-center rounded-md ${isActive ? 'bg-main' : 'bg-white hover:bg-itembg'}`}
     >
-      <ListIcon height={15} className="text-defaultBlack" />
+      <ListIcon height={15} className={` ${isActive ? 'text-white' : 'text-defaultBlack'}`} />
     </button>
   );
 };
