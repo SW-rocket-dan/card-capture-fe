d86b55c4568ba3445fd9eecc2e48dcfa96b669cb
yui880 <kimyuna521@gmail.com>
2024-10-11 15:13:42 +0900

    refactor(Banner): 로그인 후에도 체험 가능하게 변경

--- card-capture/src/components/main/Banner/Banner.tsx
+++ card-capture/src/components/main/Banner/Banner.tsx
@@ -2,18 +2,14 @@ import BannerBg from '@/components/main/Banner/components/BannerBg/BannerBg';
 import BannerText from '@/components/main/Banner/components/BannerText/BannerText';
 import BannerButton from '@/components/main/Banner/components/BannerButton/BannerButton';
 import BannerImage from '@/components/main/Banner/components/BannerImage/BannerImage';
-import { authUtils } from '@/utils';
-import PreLoginBannerButton from '@/components/main/Banner/components/BannerButton/PreLoginBannerButton';
 
 const Banner = () => {
-  const isLoggedIn = authUtils.getIsLoggedIn();
-
   return (
     <div className="relative flex h-fit w-full items-center justify-center overflow-hidden px-6 md:h-dvh">
       <BannerBg />
       <div className="flex w-fit flex-col items-center justify-center gap-[30px] py-[120px] 2xl:gap-[50px]">
         <BannerText />
-        {isLoggedIn ? <BannerButton /> : <PreLoginBannerButton />}
+        <BannerButton />
         <BannerImage />
       </div>
     </div>
--- card-capture/src/components/main/Banner/components/BannerButton/BannerButton.tsx
+++ card-capture/src/components/main/Banner/components/BannerButton/BannerButton.tsx
@@ -2,6 +2,8 @@ import StarsIcon from '@/components/common/Icon/StarsIcon';
 import useAmplitudeContext from '@/hooks/useAmplitudeContext';
 import Link from 'next/link';
 import { authUtils } from '@/utils';
+import EditIcon from '@/components/common/Icon/EditIcon';
+import useIsMobile from '@/hooks/useIsMobile';
 
 const BannerButton = () => {
   const { trackAmplitudeEvent } = useAmplitudeContext();
@@ -18,17 +20,34 @@ const BannerButton = () => {
    */
   const isLoggedIn = authUtils.getIsLoggedIn();
 
+  const { isMobile } = useIsMobile();
+
   return (
-    <Link href={isLoggedIn ? '/prompt' : '/login?create=true'}>
-      <div
-        onClick={clickHandler}
-        className="flex cursor-pointer flex-row items-center justify-center gap-1 rounded-[40px] bg-main px-[28px] py-[13px] md:px-[32px] md:py-[15px] lg:px-[38px] lg:py-[18px]"
-        style={{ boxShadow: '0 0 14px rgba(111, 108, 255, 0.5)' }}
-      >
-        <p className="text-[13px] font-medium text-white md:text-[14px] lg:text-[15px]">카드뉴스 AI로 제작하기</p>
-        <StarsIcon width={18} className="text-white" />
-      </div>
-    </Link>
+    <div className="flex flex-row gap-4">
+      {!isMobile && (
+        <Link href={'/editor/sample'}>
+          <div
+            onClick={clickHandler}
+            className="flex w-[197px] cursor-pointer flex-row items-center justify-center gap-2 rounded-[40px] bg-white px-[28px] py-[13px] md:w-[214px] md:py-[15px] lg:w-[235.8px] lg:py-[18px]"
+            style={{ boxShadow: '0 0 14px rgba(111, 108, 255, 0.3)' }}
+          >
+            <p className="text-[13px] font-semibold text-main md:text-[14px] lg:text-[15px]">에디터 체험하기</p>
+            <EditIcon width={18} className="text-main" />
+          </div>
+        </Link>
+      )}
+
+      <Link href={isLoggedIn ? '/prompt' : '/login?create=true'}>
+        <div
+          onClick={clickHandler}
+          className="flex cursor-pointer flex-row items-center justify-center gap-1 rounded-[40px] bg-main px-[28px] py-[13px] md:px-[32px] md:py-[15px] lg:px-[38px] lg:py-[18px]"
+          style={{ boxShadow: '0 0 14px rgba(111, 108, 255, 0.5)' }}
+        >
+          <p className="text-[13px] font-medium text-white md:text-[14px] lg:text-[15px]">카드뉴스 AI로 제작하기</p>
+          <StarsIcon width={18} className="text-white" />
+        </div>
+      </Link>
+    </div>
   );
 };
 
