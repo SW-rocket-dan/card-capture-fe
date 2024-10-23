b2aef9d5173920b8e24e00711de9d924bde07154
yui880 <kimyuna521@gmail.com>
2024-10-23 19:18:20 +0900

    feat(LayerListItem): 클릭시 레이어에 포커스 되도록 구현, 포커스 아이템은 배경색 다르게 설정

--- card-capture/src/components/editor/EditingArea/views/components/LayerListBox/components/LayerListItem.tsx
+++ card-capture/src/components/editor/EditingArea/views/components/LayerListBox/components/LayerListItem.tsx
@@ -2,14 +2,26 @@ import EyeIcon from '@/components/common/Icon/EyeIcon';
 import { AlignJustify } from 'lucide-react';
 import { Layer } from '@/store/useCardsStore/type';
 import TrashIcon from '@/components/common/Icon/TrashIcon';
+import { useFocusStore } from '@/store/useFocusStore';
 
 type LayerListItemProps = {
   layer: Layer;
 };
 
 const LayerListItem = ({ layer }: LayerListItemProps) => {
+  const focusedLayerId = useFocusStore(state => state.focusedLayerId);
+  const setFocusedLayerId = useFocusStore(state => state.setFocusedLayerId);
+  const isFocused = focusedLayerId === layer.id;
+
+  const focusLayerHandler = () => {
+    setFocusedLayerId(layer.id);
+  };
+
   return (
-    <div className="flex w-full flex-row items-center gap-3 rounded border border-border px-2.5 py-[8px]">
+    <div
+      className={`flex w-full flex-row items-center gap-3 rounded border border-border px-2.5 py-[8px] ${isFocused ? 'bg-light-main' : 'bg-white'}`}
+      onDoubleClick={focusLayerHandler}
+    >
       <div className="flex flex-row gap-2">
         <EyeIcon width={13} />
         <TrashIcon width={13} />
