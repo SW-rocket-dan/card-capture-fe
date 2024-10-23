1d91d80bf1171b5d1141985e2d06bde8bdb32c1b
yui880 <kimyuna521@gmail.com>
2024-10-23 19:24:03 +0900

    feat(LayerListItem): 레이어 삭제 기능 추가

--- card-capture/src/components/editor/EditingArea/views/components/LayerListBox/LayerListBox.tsx
+++ card-capture/src/components/editor/EditingArea/views/components/LayerListBox/LayerListBox.tsx
@@ -10,11 +10,11 @@ const LayerListBox = ({ card }: LayerListBoxProps) => {
   const layers = [...card.layers].sort((a, b) => b.position.zIndex - a.position.zIndex);
 
   return (
-    <div className="mt-[48px] h-[300px] w-[240px] rounded-[15px] bg-white drop-shadow">
+    <div className="mt-[48px] flex h-[300px] w-[240px] flex-col rounded-[15px] bg-white drop-shadow">
       <div className="border-b border-border px-[15px] py-[9px] text-[12px] font-medium">레이어</div>
-      <div className="flex flex-col gap-1 overflow-y-scroll px-[10px] py-[9px]">
+      <div className="my-[9px] flex flex-1 flex-col gap-1 overflow-y-scroll px-[10px]">
         {layers.map(layer => (
-          <LayerListItem layer={layer} />
+          <LayerListItem cardId={card.id} layer={layer} />
         ))}
       </div>
     </div>
--- card-capture/src/components/editor/EditingArea/views/components/LayerListBox/components/LayerListItem.tsx
+++ card-capture/src/components/editor/EditingArea/views/components/LayerListBox/components/LayerListItem.tsx
@@ -3,18 +3,27 @@ import { AlignJustify } from 'lucide-react';
 import { Layer } from '@/store/useCardsStore/type';
 import TrashIcon from '@/components/common/Icon/TrashIcon';
 import { useFocusStore } from '@/store/useFocusStore';
+import React from 'react';
+import { useCardsStore } from '@/store/useCardsStore';
 
 type LayerListItemProps = {
+  cardId: number;
   layer: Layer;
 };
 
-const LayerListItem = ({ layer }: LayerListItemProps) => {
+const LayerListItem = ({ cardId, layer }: LayerListItemProps) => {
   const focusedLayerId = useFocusStore(state => state.focusedLayerId);
   const setFocusedLayerId = useFocusStore(state => state.setFocusedLayerId);
   const isFocused = focusedLayerId === layer.id;
 
   const focusLayerHandler = () => {
     setFocusedLayerId(layer.id);
+  };
+
+  const deleteLayer = useCardsStore(state => state.deleteLayer);
+
+  const deleteLayerHandler = () => {
+    deleteLayer(cardId, layer.id);
   };
 
   return (
@@ -24,11 +33,13 @@ const LayerListItem = ({ layer }: LayerListItemProps) => {
     >
       <div className="flex flex-row gap-2">
         <EyeIcon width={13} />
-        <TrashIcon width={13} />
+        <button onClick={deleteLayerHandler}>
+          <TrashIcon width={13} />
+        </button>
       </div>
 
       <div className="flex flex-1 flex-row items-center gap-3">
-        <div className="h-8 w-8 border border-border bg-bannerbg"></div>
+        <div className="h-8 w-8 border border-border bg-white"></div>
         <p className="text-xs">Layer {layer.id + 1}</p>
       </div>
       <AlignJustify width={13} className="text-gray2" />
