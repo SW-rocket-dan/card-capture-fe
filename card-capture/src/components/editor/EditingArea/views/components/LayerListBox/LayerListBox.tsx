6828bcb30338556f1ed83f368dd7f0713ccd78df
yui880 <kimyuna521@gmail.com>
2024-10-23 18:14:03 +0900

    feat(LayerListBox): 레이어 리스트 컴포넌트 퍼블리싱

---
+++ card-capture/src/components/editor/EditingArea/views/components/LayerListBox/LayerListBox.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LayerListBox = () => {
+  return (
+    <div className="mt-[48px] h-[300px] w-[240px] rounded-[15px] bg-white drop-shadow">
+      <div className="border-b border-border px-[15px] py-[9px] text-[13px] font-medium">레이어</div>
+    </div>
+  );
+};
+
+export default LayerListBox;
