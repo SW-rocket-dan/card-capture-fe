6b6d5e8ee541cf2f818b809f3c22925849d15ea8
yui880 <kimyuna521@gmail.com>
2024-10-29 12:24:29 +0900

    feat(Promotion): 툴팁으로 설명 추가

--- card-capture/src/components/main/Promotion/Promotion.tsx
+++ card-capture/src/components/main/Promotion/Promotion.tsx
@@ -1,5 +1,6 @@
 import { createPortal } from 'react-dom';
 import GiftIcon from '@/components/common/Icon/GiftIcon';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 
 const FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSf0tw7G5pugJkqACyWcTSmIcYEPAzIsgygLlPpTCB0QO5vuhQ/viewform';
 const OPEN_CHAT_URL = 'https://open.kakao.com/o/gmoTa9Wg';
@@ -10,19 +11,36 @@ const Promotion = () => {
   };
 
   return createPortal(
-    <div className="fixed bottom-[93px] right-6 flex flex-col gap-[14px]">
-      <button
-        className="flex h-[56px] w-[56px] items-center justify-center rounded-bl-[24px] rounded-br-[24px] rounded-tl-[24px] rounded-tr-[24px] border-[0.1px] border-border bg-heart"
-        onClick={() => openEventFormHandler(FORM_URL)}
-      >
-        <GiftIcon width={30} className="text-white" />
-      </button>
-      <button
-        className="h-[56px] w-[56px] overflow-hidden rounded-bl-[24px] rounded-br-[24px] rounded-tl-[24px] rounded-tr-[24px] border-[0.1px] border-border"
-        onClick={() => openEventFormHandler(OPEN_CHAT_URL)}
-      >
-        <img alt="kakao" src="/image/kakao_logo.png" className="h-full w-full" />
-      </button>
+    <div className="fixed bottom-[95px] right-6 flex flex-col gap-[14px] font-Pretendard">
+      <TooltipProvider>
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <button
+              className="flex h-[56px] w-[56px] items-center justify-center rounded-bl-[24px] rounded-br-[24px] rounded-tl-[24px] rounded-tr-[24px] border-[0.1px] border-border bg-heart shadow-drop-medium"
+              onClick={() => openEventFormHandler(FORM_URL)}
+            >
+              <GiftIcon width={30} className="text-white" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent side="right" sideOffset={10}>
+            <p className="text-[13px] font-normal">피드백 남기고 선물 받아가세요!</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip delayDuration={0}>
+          <TooltipTrigger asChild>
+            <button
+              className="h-[56px] w-[56px] overflow-hidden rounded-bl-[24px] rounded-br-[24px] rounded-tl-[24px] rounded-tr-[24px] border-[0.1px] border-border shadow-drop-medium"
+              onClick={() => openEventFormHandler(OPEN_CHAT_URL)}
+            >
+              <img alt="kakao" src="/image/kakao_logo.png" className="h-full w-full" />
+            </button>
+          </TooltipTrigger>
+          <TooltipContent side="right" sideOffset={10}>
+            <p className="text-[13px] font-normal">오픈채팅에서 자유롭게 의견을 말해주세요!</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>,
     document.body,
   );
