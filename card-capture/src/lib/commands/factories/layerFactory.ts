028f8909c9678ffe7a14ad63141beb15c7217dc6
yui880 <kimyuna521@gmail.com>
2024-11-05 14:08:54 +0900

    feat(layerFactory): 레이어 생성 팩토리 구현

---
+++ card-capture/src/lib/commands/factories/layerFactory.ts
@@ -0,0 +1,51 @@
+import { Layer, LayerContentMap, LayerType, Position } from '@/store/useCardsStore/type';
+
+const DEFAULT_POSITION = {
+  x: 200,
+  y: 200,
+  width: 200,
+  height: 200,
+  rotate: 0,
+  opacity: 100,
+} as const;
+
+const DEFAULT_VALUES: Record<LayerType, LayerContentMap[LayerType]> = {
+  text: { content: '' },
+  shape: { type: 'rect', color: '#DDDDDD' },
+  image: {
+    url: '',
+    cropStartX: 0,
+    cropStartY: 0,
+    cropWidth: 0,
+    cropHeight: 0,
+  },
+  illust: { url: '' },
+};
+
+type BasePosition = Omit<Position, 'zIndex'>;
+
+export type LayerFactoryProps<T extends LayerType> = {
+  type: T;
+  id: number;
+  zIndex: number;
+  position?: Partial<BasePosition>;
+  content?: Partial<LayerContentMap[T]>;
+};
+
+export const LayerFactory = {
+  createLayer<T extends LayerType>({ type, id, zIndex, position, content }: LayerFactoryProps<T>): Layer {
+    return {
+      id,
+      type,
+      content: {
+        ...DEFAULT_VALUES[type],
+        ...(content as LayerContentMap[T]),
+      },
+      position: {
+        ...DEFAULT_POSITION,
+        ...position,
+        zIndex,
+      },
+    } as Layer;
+  },
+};
