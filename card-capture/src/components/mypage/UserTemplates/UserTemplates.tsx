64d99eeea6cddd6f58f83337dcfdb72ffe0007d8
yui880 <kimyuna521@gmail.com>
2024-11-22 17:58:49 +0900

    refactor(UserTemplate): 데이터 외부에서 받도록 변경

--- card-capture/src/components/mypage/UserTemplates/UserTemplates.tsx
+++ card-capture/src/components/mypage/UserTemplates/UserTemplates.tsx
@@ -1,15 +1,17 @@
 import UserTemplateDetails from '@/components/mypage/UserTemplates/components/UserTemplateDetails';
-import { useQuery } from '@tanstack/react-query';
-import { templateApi } from '@/api';
 import { TemplateList } from '@/types';
 import Title from '@/components/common/Title/Title';
 
-const UserTemplates = () => {
-  const { data } = useQuery<TemplateList>({ queryKey: ['all-template'], queryFn: templateApi.getAllTemplateData });
+type UserTemplatesProps = {
+  data: TemplateList | undefined;
+  title: string;
+  content: string;
+};
 
+const UserTemplates = ({ data, title, content }: UserTemplatesProps) => {
   return (
     <div className="flex h-full w-full flex-col">
-      <Title title="내 템플릿 보기" content="내가 만든 템플릿을 모아서 볼 수 있어요!" />
+      <Title title={title} content={content} />
 
       <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-x-4 gap-y-7 px-6 py-12 sm:grid-cols-2 sm:px-8 md:px-12 lg:grid-cols-3 lg:px-14 xl:grid-cols-4">
         {data?.map(template => <UserTemplateDetails key={template.id} template={template} />)}
